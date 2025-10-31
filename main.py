e2be502c94512d54b3605f1af1e6e12e6b75c7c4
Kbot64 <ktsky515@gmail.com>
2025-10-30 20:04:53 -0500

    Finished required functions for bot completing project

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
+import sys
 from stats import book_word_count
 from stats import num_of_each_char
+from stats import make_sorted_dict_list
 
 def get_book_contents(filepath):
     with open(filepath) as f:
         file_contents = f.read()
     return file_contents
 
+def print_report(filepath):
+    
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {filepath}")
+    print("----------- Word Count ----------")
+    print(f"Found {book_word_count(get_book_contents(filepath))}")
+    book_character_dictionary = num_of_each_char(get_book_contents(filepath))
+    print("--------- Character Count -------")
+    report_list = make_sorted_dict_list(book_character_dictionary)
+    for dict in report_list:
+        for key in dict:
+            if key == "char" and dict["char"].isalpha():
+                print(f"{dict["char"]}: {dict["num"]}")
+    print("============= END ===============")
+        
 
 
 def main():
-    # print(get_book_contents("books/frankenstein.txt"))
-    print(f"Found {book_word_count(get_book_contents("books/frankenstein.txt"))} total words")
-    book_chars_dict = num_of_each_char(get_book_contents("books/frankenstein.txt"))
-    print(book_chars_dict)
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    print(f"Found {book_word_count(get_book_contents(sys.argv[1]))} total words")
+    book_chars_dict = num_of_each_char(get_book_contents(sys.argv[1]))
+    # print(book_chars_dict)
+    report_list = make_sorted_dict_list(book_chars_dict)
+    print_report(sys.argv[1])
 main()
--- stats.py
+++ stats.py
@@ -14,3 +14,19 @@ def num_of_each_char(book_string):
                 char_dict[l.lower()] = 1
         
     return char_dict
+
+def get_num(dictionary):
+    return dictionary["num"]
+
+
+
+def make_sorted_dict_list(dictionary):
+    list_of_dictionaries = []
+    for k in dictionary:
+        temp_dictionary = {}
+        temp_dictionary["char"] = k
+        temp_dictionary["num"] = dictionary[k]
+        list_of_dictionaries.append(temp_dictionary)
+        list_of_dictionaries.sort(reverse=True, key=get_num)
+    return list_of_dictionaries
+
